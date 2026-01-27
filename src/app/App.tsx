5871e8fb74fc22f8f0a8ecf1aa6c19b149616772
Seah Kim <seahkeem@gmail.com>
2026-01-27 16:45:35 +0900

    Chore: set up React environment

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-50">
+      <h1 className="text-5xl font-black text-blue-600 tracking-tighter">
+        토독
+      </h1>
+    </main>
+  )
+}
+
+export default App
