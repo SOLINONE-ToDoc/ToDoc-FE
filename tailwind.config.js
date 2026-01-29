a1443fab018da76f5baf661be4ca65bf7ad07488
Seah Kim <seahkeem@gmail.com>
2026-01-29 23:37:47 +0900

    [#3] Design : 시맨틱 컬러 토큰 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,40 @@ export default {
           500: '#606060',
           600: '#404040',
         },
+
+        surface: {
+          base: '#ffffff',
+          surface: '#f6f6f6',
+          primary: '#000000',
+          secondary: '#e9e9e9',
+          tooltip: '#404040',
+          input: '#ffffff',
+          overlay: 'rgba(0,0,0,0.7)'
+        },
+
+        content: {
+          primary: '#000000',
+          secondaryMain: '#000000',
+          secondarySub: '#606060',
+          muted: '#8b8b8b',
+
+          navActive: '#000000',
+          navInactive: '#8b8b8b',
+
+          placeholder: '#d9d9d9',
+
+          onPrimary: '#000000',
+          onOverlay: '#FFFFFF',
+
+          success: '#40a6eb',
+          error: '#ff6666',
+          warning: '#ff6666',
+        },
+
+        line: {
+          default: '#e9e9e9',
+          strong: '#d9d9d9',
+        },
       },
 
       fontFamily: tailwindFontFamily,
