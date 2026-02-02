757a1adb50b5cdd2c16e636104be330a9467bf1a
Seah Kim <seahkeem@gmail.com>
2026-02-02 16:44:04 +0900

    [#4] Feat: 로그인 페이지 내 LoginForm 컴포넌트 구현 및 배치

---
+++ src/pages/LoginPage/ui/LoginForm.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Input } from '@/shared/ui/Input';
+import { Button } from '@/shared/ui/Button';
+
+type LoginFormData = {
+  email: string;
+  password: string;
+};
+
+interface LoginFormProps {
+  formData: LoginFormData;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onSubmit: (formData: LoginFormData) => void;
+}
+
+export const LoginForm: React.FC<LoginFormProps> = ({ formData, onChange, onSubmit }) => {
+  return (
+    <form className="flex flex-col w-full" noValidate onSubmit={(e) => { e.preventDefault(); onSubmit(formData); }}>
+      <div className="flex flex-col gap-2 lg:gap-8">
+        <Input
+          label="이메일 아이디"
+          labelClassName="text-[24px] hidden lg:block"
+          name="email"
+          type="email"
+          placeholder="이메일을 입력해주세요"
+          value={formData.email}
+          onChange={onChange}
+          required
+        />
+
+        <div className="flex flex-col gap-2 lg:gap-3">
+          <Input
+            label="비밀번호"
+            labelClassName="text-[24px] hidden lg:block"
+            name="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            allowToggle
+            value={formData.password}
+            onChange={onChange}
+            required
+          />
+          <div className="flex items-center justify-end gap-3">
+            <Button
+              type="button"
+              variant="text"
+              size="text"
+              onClick={() => console.log('아이디 찾기')}
+              >
+              아이디 찾기
+            </Button>
+
+            <Button
+              type="button"
+              variant="text"
+              size="text"
+              onClick={() => console.log('비밀번호 찾기')}
+              >
+              비밀번호 찾기
+            </Button>
+
+            <Button
+              type="button"
+              variant="text"
+              size="text"
+              onClick={() => console.log('회원가입')}
+              >
+              회원가입
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 lg:gap-4 mt-8 lg:mt-20">
+        <Button
+          type="submit"
+          variant="primary"
+          size="xl"
+          fullWidth
+          >
+          로그인
+        </Button>
+
+        <Button
+          type="button"
+          variant="secondary"
+          size="xl"
+          fullWidth
+        >
+          비회원으로 작성하기
+        </Button>
+      </div>
+    </form>
+  );
+};
