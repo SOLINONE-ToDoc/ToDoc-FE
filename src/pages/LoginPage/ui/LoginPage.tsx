5ae094ec5ebd92f6d63222f313c850085f3d0b9d
Seah Kim <seahkeem@gmail.com>
2026-02-02 16:44:04 +0900

    [#4] Feat: 로그인 페이지 내 LoginForm 컴포넌트 구현 및 배치

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import type { LoginRequest } from '@/features/auth'
+import { LoginForm } from './LoginForm';
+
 export const LoginPage = () => {
+
+  const [formData, setFormData] = useState<Pick<LoginRequest, 'email' | 'password'>>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormData(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (data: { email: string; password: string }) => {
+    console.log('로그인 요청', data);
+  };
+
   return (
      <main className="flex flex-col items-center px-5 w-full max-w-[750px] mx-auto min-h-screen pt-[168px]">
+      <LoginForm
+        formData={formData}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </main>
   );
 };
