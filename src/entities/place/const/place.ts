331ce36dc85ce2874d76edc2889becaec952963f
Seah Kim <seahkeem@gmail.com>
2026-02-09 18:09:51 +0900

    [#15] Feat: 장소 매핑 테이블 추가

---
+++ src/entities/place/const/place.ts
@@ -0,0 +1,10 @@
+import type { PlaceType } from "../model/types";
+
+export const PLACE_TYPE_LABEL: Record<PlaceType, string> = {
+  RESTAURANT: '음식점',
+  CAFE: '카페',
+  BANK: '은행',
+  MART: '마트',
+  CONVENIENCE_STORE: '편의점',
+  OTHER: '기타',
+};
