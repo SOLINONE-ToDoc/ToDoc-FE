c02ac9dbbc1d2723db8ef8bf418ebe49c1d74bba
Seah Kim <seahkeem@gmail.com>
2026-02-11 04:17:30 +0900

    Feat: 산돌 웹폰트 적용 - 배포 서버에서 확인

--- src/widgets/home/IntroSection.tsx
+++ src/widgets/home/IntroSection.tsx
@@ -18,7 +18,7 @@ export const IntroSection = () => {
     <section className="flex flex-col items-center mt-[108px] py-[102px]">
       <div className="w-full flex flex-col items-center gap-4 lg:gap-10 text-center">
         <div className="flex flex-col gap-3 items-center">
-          <span className="text-[18px] font-semibold text-gray-500" style={{fontFamily: "Sandoll Geobok"}}>
+          <span className="text-[18px] font-semibold text-gray-500" style={{ fontFamily: "'Sandoll Geobok'", fontWeight: 400}}>
             추억을 남기는 가장 쉬운 방명록
           </span>
           <span
