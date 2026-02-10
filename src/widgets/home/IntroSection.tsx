38ca5bd29eb62380daf9ba4dc1cd67e2a4b7fbcf
Seah Kim <seahkeem@gmail.com>
2026-02-11 01:22:21 +0900

    Feat: 산돌 폰트 배포 서버 적용 테스트 - 산돌 구름 웹폰트 적용 확인용 - 로컬에서는 SID 제한으로 적용 불가

--- src/widgets/home/IntroSection.tsx
+++ src/widgets/home/IntroSection.tsx
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/shared/ui/Button";
 import { ICONS } from "@/shared/constants";
+import { loadWebFont } from "@/entities/font/hooks/loadWebFont";
 
 export const IntroSection = () => {
   const navigate = useNavigate();
+  const linkHref = "[https://qns2c88qif.execute-api.ap-northeast-2.amazonaws.com/v1/api/css/drop_fontstream_css/?sid=gAAAAABpgEQse68YztzGZ_eK8S6TbFLggmoCHvUKY-KjX_w6d0UBXvaEOYMsoPuVHoriIc-syH0Wuyvqss8bLnJJejjr5sm-dED7bmR9XyiE6DXWde68d2qkhUhAf36kHDmBpiAqnvtAuUPWF_gBkFWf6yuUk2_lswQhdlS5dwXUpfU0jkT_H9fTauDXVjEFjUiceVde8t-tzPVMq31PB3sV77b4AZ8CnWGUEpmaTdsiv32mhrcClsxGxKgQFP7GYLEA3snTm1nL](https://qns2c88qif.execute-api.ap-northeast-2.amazonaws.com/v1/api/css/drop_fontstream_css/?sid=gAAAAABpgEQse68YztzGZ_eK8S6TbFLggmoCHvUKY-KjX_w6d0UBXvaEOYMsoPuVHoriIc-syH0Wuyvqss8bLnJJejjr5sm-dED7bmR9XyiE6DXWde68d2qkhUhAf36kHDmBpiAqnvtAuUPWF_gBkFWf6yuUk2_lswQhdlS5dwXUpfU0jkT_H9fTauDXVjEFjUiceVde8t-tzPVMq31PB3sV77b4AZ8CnWGUEpmaTdsiv32mhrcClsxGxKgQFP7GYLEA3snTm1nL";
+
+  useEffect(() => {
+    loadWebFont(linkHref)
+      .then(() => console.log("success"))
+      .catch((e) => console.log("error", e));
+  }, []);
 
   return (
     <section className="flex flex-col items-center mt-[108px] py-[102px]">
       <div className="w-full flex flex-col items-center gap-4 lg:gap-10 text-center">
         <div className="flex flex-col gap-3 items-center">
-          <span className="text-[18px] font-semibold text-gray-500">
+          <span className="text-[18px] font-semibold text-gray-500" style={{fontFamily: "Sandoll Geobok"}}>
             추억을 남기는 가장 쉬운 방명록
           </span>
           <span
@@ -22,9 +31,9 @@ export const IntroSection = () => {
         </div>
         <Button
           variant="primary" size="lPill" type="button"
-          rightIcon={<ICONS.Down width={24} height={24} color="white" strokeWidth={2} className="-rotate-90"/>}
+          rightIcon={<ICONS.Down width={24} height={24} color="white" strokeWidth={2} className="-rotate-90" />}
           onClick={() => navigate('/login')}
-          >
+        >
           무료로 방명록 만들기
         </Button>
       </div>
